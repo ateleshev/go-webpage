75ee3c0e78670122b4d1661560bb9bcffbe3384f
Artem Teleshev <artem.teleshev@gmail.com>
2015-11-28 16:12:16 +0200

    Added ability to delete item from PageData and clearing PageData

--- PageData.go
+++ PageData.go
@@ -28,3 +28,17 @@ func (this *PageData) Set(name string, data interface{}) bool { // {{{
 func (this *PageData) Get(name string) interface{} { // {{{
 	return (*this)[name]
 } // }}}
+
+func (this *PageData) Del(name string) bool { // {{{
+	delete((*this), name)
+
+	return true
+} // }}}
+
+func (this *PageData) Clear() bool { // {{{
+	for name, _ := range *this {
+		this.Del(name)
+	}
+
+	return true
+} // }}}
